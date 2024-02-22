df1964a177ed8ee45e6640fc933313cbde5e721f
Samuel Bonet <samuelbonetweb@gmail.com>
2024-02-22 18:57:53 +0100

    updatePiano

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const pianoTeclas = document.querySelectorAll('.tecla') // Seleciona todas las teclas 
+const pianoTeclas = document.querySelectorAll('.tecla'); // Selecciona todas las teclas 
 
-function playSound(newUrl) {    // Función para reproducir el sonido
-    console.log(newUrl) // Imprime la url del sonido
-    new Audio(newUrl).play()    // Reproduce el sonido
+function playSound(newUrl) { // Función para reproducir el sonido
+    console.log(newUrl); // Imprime la url del sonido
+    new Audio(newUrl).play(); // Reproduce el sonido
 }
 
-
-pianoTeclas.forEach((pianoTeclas, i) => {   // Recorre todas las teclas
-    const numero = i < 9 ? '0' + (i + 1) : (i + 1)  // Si el número es menor a 9, le agrega un 0 delante
-    const newUrl = 'sounds/key' + numero + '.mp3'    // Crea la url del sonido
-    pianoTeclas.addEventListener('click', () => playSound(newUrl))  // Agrega un evento click a cada tecla
-})
+pianoTeclas.forEach((tecla, i) => { // Recorre todas las teclas
+    const numero = i < 9 ? '0' + (i + 1) : (i + 1); // Si el número es menor a 9, le agrega un 0 delante
+    const newUrl = 'sounds/key' + numero + '.mp3'; // Crea la url del sonido
+    tecla.addEventListener('click', () => playSound(newUrl)); // Agrega un evento click a cada tecla
+});
